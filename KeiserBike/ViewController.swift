3c0e2fbf9a9b0e49ded6e7ce7285fb0cf93ee5a4
Brandon Roth <brandon@rocketmade.com>
2015-01-09 15:19:52 -0700

    Initial Commit

---
+++ KeiserBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeiserBike
+//
+//  Created by Brandon Roth on 1/9/15.
+//  Copyright (c) 2015 Rocketmade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
